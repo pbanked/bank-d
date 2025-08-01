668d21ec95155e9e89e9f13de416553066ec7fd7
joeulam <joeulam@bu.edu>
2025-08-01 00:03:29 -0400

    added mantine UI and construction page

---
+++ app/provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { MantineProvider } from "@mantine/core";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <MantineProvider
+      defaultColorScheme="light" 
+      theme={{
+        primaryColor: "blue", 
+      }}
+    >
+      {children}
+    </MantineProvider>
+  );
+}
