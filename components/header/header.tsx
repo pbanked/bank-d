8023c4efa27e514712e97b78100af64695afcc08
Joey Lam <56980755+joeulam@users.noreply.github.com>
2025-10-27 18:49:46 -0400

    changed file layout (#15)
    Updated nav to redirect
    Added layout to each page
    Moved files to [user]

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,21 +1,48 @@
-import { Avatar, Box, Burger, Group, Title } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { IconBell } from "@tabler/icons-react";
+import {
+  Avatar,
+  Box,
+  Group,
+  Menu,
+  Switch,
+  Title,
+} from "@mantine/core";
+import {
+  IconBell,
+  IconMessageCircle,
+  IconSettings,
+} from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
 
 export default function Header({ headerTitle }: { headerTitle: string }) {
-  const [opened, { toggle }] = useDisclosure();
+  const router = useRouter()
   return (
     <Box style={{ width: "90vw", zIndex: 1000, padding: "1rem 0" }}>
       <Group justify="space-between" align="center">
         <Title order={1}>{headerTitle}</Title>
 
         <Group>
-          <Burger
-            opened={opened}
-            onClick={toggle}
-            aria-label="Toggle navigation"
-          />
-          <Avatar radius="xl" />
+          <Menu shadow="md" width={200}>
+            <Menu.Target>
+              <Avatar className="cursor-pointer" radius="xl" />
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Label>Application</Menu.Label>
+              <Menu.Item leftSection={<IconSettings size={14} />} onClick={() => router.push("account")}>
+                Settings
+              </Menu.Item>
+              <Menu.Item leftSection={<IconMessageCircle size={14} />}>
+                Messages
+              </Menu.Item>
+              <Menu.Divider />
+
+              <Menu.Label>Visual</Menu.Label>
+              <Menu.Item>
+                <Switch label="Light/Dark mode" />
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+
           <IconBell />
         </Group>
       </Group>
